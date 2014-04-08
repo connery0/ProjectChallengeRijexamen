2f5369b3f01cc634c86c4b26e1671fc4d6729913
connery0 <onmymarkburn4321burn@gmail.com>
2014-04-08 16:32:08 +0200

    Random multiple choice

--- ProjectChallengeRijexamen/Form1.Designer.cs
+++ ProjectChallengeRijexamen/Form1.Designer.cs
@@ -28,57 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.antwoord1 = new System.Windows.Forms.RadioButton();
+            this.antwoord2 = new System.Windows.Forms.RadioButton();
+            this.antwoord3 = new System.Windows.Forms.RadioButton();
+            this.antwoord4 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
+            this.Vraagbox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // antwoord1
+            // 
+            this.antwoord1.AutoSize = true;
+            this.antwoord1.Location = new System.Drawing.Point(12, 76);
+            this.antwoord1.Name = "antwoord1";
+            this.antwoord1.Size = new System.Drawing.Size(79, 17);
+            this.antwoord1.TabIndex = 0;
+            this.antwoord1.TabStop = true;
+            this.antwoord1.Text = "Antwoord 1";
+            this.antwoord1.UseVisualStyleBackColor = true;
+            // 
+            // antwoord2
+            // 
+            this.antwoord2.AutoSize = true;
+            this.antwoord2.Location = new System.Drawing.Point(12, 109);
+            this.antwoord2.Name = "antwoord2";
+            this.antwoord2.Size = new System.Drawing.Size(79, 17);
+            this.antwoord2.TabIndex = 1;
+            this.antwoord2.TabStop = true;
+            this.antwoord2.Text = "Antwoord 2";
+            this.antwoord2.UseVisualStyleBackColor = true;
+            // 
+            // antwoord3
+            // 
+            this.antwoord3.AutoSize = true;
+            this.antwoord3.Location = new System.Drawing.Point(12, 137);
+            this.antwoord3.Name = "antwoord3";
+            this.antwoord3.Size = new System.Drawing.Size(79, 17);
+            this.antwoord3.TabIndex = 2;
+            this.antwoord3.TabStop = true;
+            this.antwoord3.Text = "Antwoord 3";
+            this.antwoord3.UseVisualStyleBackColor = true;
+            // 
+            // antwoord4
+            // 
+            this.antwoord4.AutoSize = true;
+            this.antwoord4.Location = new System.Drawing.Point(12, 164);
+            this.antwoord4.Name = "antwoord4";
+            this.antwoord4.Size = new System.Drawing.Size(79, 17);
+            this.antwoord4.TabIndex = 3;
+            this.antwoord4.TabStop = true;
+            this.antwoord4.Text = "Antwoord 4";
+            this.antwoord4.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 67);
+            this.button1.Location = new System.Drawing.Point(199, 227);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(47, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "controleer / volgende";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Vraagbox
+            // 
+            this.Vraagbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vraagbox.Location = new System.Drawing.Point(2, 2);
+            this.Vraagbox.Multiline = true;
+            this.Vraagbox.Name = "Vraagbox";
+            this.Vraagbox.ReadOnly = true;
+            this.Vraagbox.Size = new System.Drawing.Size(323, 70);
+            this.Vraagbox.TabIndex = 5;
+            this.Vraagbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Moire", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(162, 40);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Juist";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(195, 67);
+            this.button2.Location = new System.Drawing.Point(199, 198);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(49, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Start / reset";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(46, 162);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(198, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(330, 262);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Vraagbox);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.antwoord4);
+            this.Controls.Add(this.antwoord3);
+            this.Controls.Add(this.antwoord2);
+            this.Controls.Add(this.antwoord1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.RadioButton antwoord1;
+        private System.Windows.Forms.RadioButton antwoord2;
+        private System.Windows.Forms.RadioButton antwoord3;
+        private System.Windows.Forms.RadioButton antwoord4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Vraagbox;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+
     }
 }
 
--- ProjectChallengeRijexamen/Form1.cs
+++ ProjectChallengeRijexamen/Form1.cs
@@ -12,9 +12,69 @@ namespace ProjectChallengeRijexamen
 {
     public partial class Form1 : Form
     {
+        int vraag = -1;
+        Random r = new Random();
+        String[][] AntwoordArray;
+        String[][] AntwoordenLijst = 
+            {
+                new String[] {"1+1=","2","3","7","9"},
+                new String[] {"0+0","0","4","6","984512316546531"},
+                new String[] {"test","Juist Antwoord","11","22","1531"},
+                new String[] {"2+2=","4","11","22","1531"}
+            };
+
+
+
+        private String[][] DeleteRow(String[][] A, int ToDelete)
+        {
+            String[][] TempArray = new String[A.Length - 1][];
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i < ToDelete)
+                {
+                    TempArray[i] = A[i];
+                }
+                else if (i > ToDelete)
+                {
+                    TempArray[i - 1] = A[i];
+                }
+            }
+
+
+
+            return TempArray;
+        }
+
+
+        private void genereerVraag()
+        {
+            vraag = r.Next(0, AntwoordArray.Length);
+            Vraagbox.Text = AntwoordArray[vraag][0];
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (vraag > -1 && AntwoordArray.Length > 1)
+            {
+
+                AntwoordArray = DeleteRow(AntwoordArray, vraag);
+                genereerVraag();
+            }
+            else { MessageBox.Show("Einde"); }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AntwoordArray = AntwoordenLijst;
+            genereerVraag();
         }
     }
 }
