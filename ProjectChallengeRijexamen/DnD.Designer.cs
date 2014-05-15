5310cfedf90aa81a9a540833f9d8ec681af8bc3c
MrInvisible99 <theinvisible99@hotmail.com>
2014-05-15 17:08:28 +0200

    azeaea

--- ProjectChallengeRijexamen/DnD.Designer.cs
+++ ProjectChallengeRijexamen/DnD.Designer.cs
@@ -137,7 +137,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F);
-            this.button1.Location = new System.Drawing.Point(12, 449);
+            this.button1.Location = new System.Drawing.Point(12, 436);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 45);
             this.button1.TabIndex = 69;
@@ -154,6 +154,8 @@
             this.pictureBox12.Size = new System.Drawing.Size(100, 100);
             this.pictureBox12.TabIndex = 66;
             this.pictureBox12.TabStop = false;
+            this.pictureBox12.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragDrop);
+            this.pictureBox12.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragEnter);
             this.pictureBox12.DoubleClick += new System.EventHandler(this.pictureBox10_DoubleClick);
             // 
             // pictureBox11
@@ -165,6 +167,8 @@
             this.pictureBox11.Size = new System.Drawing.Size(100, 100);
             this.pictureBox11.TabIndex = 65;
             this.pictureBox11.TabStop = false;
+            this.pictureBox11.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragDrop);
+            this.pictureBox11.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragEnter);
             this.pictureBox11.DoubleClick += new System.EventHandler(this.pictureBox10_DoubleClick);
             // 
             // pictureBox10
@@ -176,6 +180,8 @@
             this.pictureBox10.Size = new System.Drawing.Size(100, 100);
             this.pictureBox10.TabIndex = 64;
             this.pictureBox10.TabStop = false;
+            this.pictureBox10.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragDrop);
+            this.pictureBox10.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragEnter);
             this.pictureBox10.DoubleClick += new System.EventHandler(this.pictureBox10_DoubleClick);
             // 
             // pictureBox9
@@ -187,6 +193,8 @@
             this.pictureBox9.Size = new System.Drawing.Size(100, 100);
             this.pictureBox9.TabIndex = 63;
             this.pictureBox9.TabStop = false;
+            this.pictureBox9.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragDrop);
+            this.pictureBox9.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragEnter);
             this.pictureBox9.DoubleClick += new System.EventHandler(this.pictureBox10_DoubleClick);
             // 
             // pictureBox8
@@ -198,6 +206,8 @@
             this.pictureBox8.Size = new System.Drawing.Size(100, 100);
             this.pictureBox8.TabIndex = 62;
             this.pictureBox8.TabStop = false;
+            this.pictureBox8.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragDrop);
+            this.pictureBox8.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragEnter);
             this.pictureBox8.DoubleClick += new System.EventHandler(this.pictureBox10_DoubleClick);
             // 
             // pictureBox7
@@ -209,6 +219,8 @@
             this.pictureBox7.Size = new System.Drawing.Size(100, 100);
             this.pictureBox7.TabIndex = 61;
             this.pictureBox7.TabStop = false;
+            this.pictureBox7.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragDrop);
+            this.pictureBox7.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox7_DragEnter);
             this.pictureBox7.DoubleClick += new System.EventHandler(this.pictureBox10_DoubleClick);
             // 
             // pictureBox1
@@ -274,7 +286,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F);
-            this.button2.Location = new System.Drawing.Point(330, 449);
+            this.button2.Location = new System.Drawing.Point(330, 436);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 45);
             this.button2.TabIndex = 69;
@@ -290,7 +302,7 @@
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.BackgroundImage = global::ProjectChallengeRijexamen.Properties.Resources.Test__750x502_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(662, 506);
+            this.ClientSize = new System.Drawing.Size(660, 489);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox2);
@@ -318,6 +330,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Verkeersborden";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DnD_FormClosing);
+            this.BackColorChanged += new System.EventHandler(this.ds);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
