e0b0c349dd4eaaffeea1acb878d35f839a6b01e4
PainToAgony <oblivion@live.be>
2014-05-16 12:08:56 +0200

    fbgm uezqeù=

--- ProjectChallengeRijexamen/Memory.Designer.cs
+++ ProjectChallengeRijexamen/Memory.Designer.cs
@@ -259,6 +259,7 @@
             this.button1.TabIndex = 18;
             this.button1.Text = "Terug";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Memory
             // 
